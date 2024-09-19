e7af82105d0ea0297ac59b962c7cb2f47b1cfe6b
lmangall <l.mangallon@gmail.com>
2024-09-19 20:18:54 +0200

    Chore: cleanup

--- tr_backend/game/models.py
+++ tr_backend/game/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 
 # the game state, scores, start/end time, players involved, and the game status.
-class Match
+#class Match
 
 # name, score, connection (if handling multiplayer over a network)...
-class Player
+#class Player
 
 # paddle's position, speed, dimensions...
 class Paddle(models.Model):
-    id = models.IntegerField()
     #position_x = models.FloatField(default=0.0)  needed ?
     position_y = models.FloatField(default=0.0)
     height = models.FloatField(default=100.0)
     width = models.FloatField(default=10.0)
 
 #  position, velocity, size
-class Ball
+#class Ball
 
 #what collided (paddle or wall), the position and time of the collision,  maybe  logic to calculate the new direction of the ball.
-class Collision
+#class Collision
 
