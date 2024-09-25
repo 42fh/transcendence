77b4f4fc8f63f2bd8626b03a227456e043113cc3
dantol29 <dantol29@gmail.com>
2024-09-25 15:20:47 +0300

    feat(db): add initial tables

--- django/game/models.py
+++ django/game/models.py
@@ -1,10 +1,36 @@
+# Contains database tables
 from django.db import models
+from django.contrib.auth.models import User
 
-# the game state, scores, start/end time, players involved, and the game status.
-#class Match
+class Profile(models.Model):
+    # OneToOneField - one user has one profile, CASCADE - if user is deleted, delete profile
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    # ManyToManyField - user can have many games
+    games = models.ManyToManyField('Game', blank=True)
+    friends = models.ManyToManyField('Profile', blank=True)
+    
+    def __str__(self):
+        return self.user.username
 
-# name, score, connection (if handling multiplayer over a network)...
-#class Player
+class Game(models.Model):
+    date = models.DateField()
+    players = models.ManyToManyField(Profile, blank=True)
+    duration = models.IntegerField()
+    mode = models.ForeignKey(GameMode)
+    # won_games = profile.games_won.all() - get all games won by profile
+    winner = models.ForeignKey(Profile, related_name='games_won', null=True, blank=True)
 
+    def __str__(self):
+        return self.mode.name + ' ' + str(self.date)
+
+class GameMode(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
