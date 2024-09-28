dace53908b25ab5efde62b54ef575412fe2578f2
Florian Held <flori4nh@gmail.com>
2024-09-28 16:00:18 +0200

    take secret key from env, add allowed host

--- backend/pong/pong/settings.py
+++ backend/pong/pong/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&gughj08gm406_xjzpqb&3z^#4%&o(n(3aw95fh-wu@x_2h%-i'
+# SECRET_KEY = 'django-insecure-&gughj08gm406_xjzpqb&3z^#4%&o(n(3aw95fh-wu@x_2h%-i'
+SECRET_KEY = os.environ["SECRET_KEY"]
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['transcendence-ctqy.onrender.com']
 
 
 # Application definition
