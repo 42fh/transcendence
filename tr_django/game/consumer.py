de5e5c60dd95d0e540a4992f5d08e3eae21b92e7
lmangall <l.mangallon@gmail.com>
2024-10-03 18:27:20 +0200

    feat: have two paddles synced

--- tr_django/game/consumer.py
+++ tr_django/game/consumer.py
@@ -13,14 +13,11 @@ class GameConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        if 'message' in text_data_json:
-            message = text_data_json['message']
-            await self.send_to_all_clients({'message': message})
-        elif 'type' in text_data_json and text_data_json['type'] == 'position':
+        if 'type' in text_data_json and text_data_json['type'] == 'position':
             position = text_data_json['position']
-            await self.send_to_all_clients({'type': 'position', 'position': position})
+            clientID = text_data_json['clientID']
+            await self.send_to_all_clients({'type': 'position', 'clientID': clientID, 'position': position})
 
     async def send_to_all_clients(self, data):
         for client in self.connected_clients:
             await client.send(text_data=json.dumps(data))
-        
