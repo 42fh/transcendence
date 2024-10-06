fecdc9701a81259aae28a78662239434d9585ea6
lmangall <l.mangallon@gmail.com>
2024-10-06 18:29:58 +0200

    Refactor(consumer): validation server side

--- tr_django/game/consumer.py
+++ tr_django/game/consumer.py
@@ -3,6 +3,8 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class GameConsumer(AsyncWebsocketConsumer):
     connected_clients = []
+    GAME_FIELD_HEIGHT = 500
+    PADDLE_HEIGHT = 100
 
     async def connect(self):
         self.connected_clients.append(self)
@@ -16,7 +18,22 @@ class GameConsumer(AsyncWebsocketConsumer):
         if 'type' in text_data_json and text_data_json['type'] == 'position':
             position = text_data_json['position']
             clientID = text_data_json['clientID']
-            await self.send_to_all_clients({'type': 'position', 'clientID': clientID, 'position': position})
+            
+            valid_position = self.validate_position(position['y'])
+            if valid_position is not None:
+                position['y'] = valid_position
+                await self.send_to_all_clients({
+                    'type': 'position',
+                    'clientID': clientID,
+                    'position': position
+                })
+
+    def validate_position(self, y):
+        if y < 0:
+            return 0  # Reset to the y
+        elif y > self.GAME_FIELD_HEIGHT - self.PADDLE_HEIGHT:
+            return self.GAME_FIELD_HEIGHT - self.PADDLE_HEIGHT 
+        return y
 
     async def send_to_all_clients(self, data):
         for client in self.connected_clients:
