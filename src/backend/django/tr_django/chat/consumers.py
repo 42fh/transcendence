d47cfc2783673adcd78e612c8d260c626d802297
lmangall <l.mangallon@gmail.com>
2024-10-17 14:01:11 +0200

    feat: add list of connected users

--- src/backend/django/tr_django/chat/consumers.py
+++ src/backend/django/tr_django/chat/consumers.py
@@ -2,30 +2,64 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    connected_users = set()
+
     async def connect(self):
         self.roomGroupName = "group_chat_gfg"
+        self.username = self.scope["user"].username
+
+        if not self.username:
+            await self.close()
+            return
+
+        ChatConsumer.connected_users.add(self.username)
+
         await self.channel_layer.group_add(
-            self.roomGroupName ,
+            self.roomGroupName,
             self.channel_name
         )
         await self.accept()
-    async def disconnect(self , close_code):
-        await self.channel.name.group_discard(
-            self.roomGroupName , 
-            self.channel_name 
+
+        await self.channel_layer.group_send(
+            self.roomGroupName,
+            {
+                "type": "user_list",
+                "users": list(ChatConsumer.connected_users)
+            }
         )
+
+    async def disconnect(self, close_code):
+        ChatConsumer.connected_users.discard(self.username)
+        await self.channel_layer.group_discard(
+            self.roomGroupName,
+            self.channel_name
+        )
+
+        await self.channel_layer.group_send(
+            self.roomGroupName,
+            {
+                "type": "user_list",
+                "users": list(ChatConsumer.connected_users)
+            }
+        )
+
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        username = text_data_json["username"]
+
         await self.channel_layer.group_send(
-            self.roomGroupName,{
-                "type" : "sendMessage" ,
-                "message" : message , 
-                "username" : username ,
-            })
-    async def sendMessage(self , event) : 
+            self.roomGroupName, {
+                "type": "sendMessage",
+                "message": message,
+                "username": self.username,
+            }
+        )
+
+    async def sendMessage(self, event):
         message = event["message"]
         username = event["username"]
-        await self.send(text_data = json.dumps({"message":message ,"username":username}))
-      
+        await self.send(text_data=json.dumps({"message": message, "username": username}))
+
+    async def user_list(self, event):
+        users = event["users"]
+        await self.send(text_data=json.dumps({"type": "user_list", "users": users}))
