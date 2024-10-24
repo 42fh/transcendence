ce6f4fff66226387ff1bcd934ad6057c2ad92975
lmangall <l.mangallon@gmail.com>
2024-10-24 02:45:40 +0200

    Fix: post merge problems

--- src/backend/django/tr_django/chat/routing.py
+++ src/backend/django/tr_django/chat/routing.py
@@ -3,9 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     path("ws/chat/", consumers.ChatConsumer.as_asgi()),
-<<<<<<< HEAD
     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
-=======
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.OneToOneChatConsumer.as_asgi()),
->>>>>>> main
 ]
