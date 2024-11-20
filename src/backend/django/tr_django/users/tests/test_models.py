530977572b110593406fe2e8f374f1c11f282954
552020 <stefanolombardo@posteo.de>
2024-11-20 18:20:14 +0100

    Chore: fix test

--- src/backend/django/tr_django/users/tests/test_models.py
+++ src/backend/django/tr_django/users/tests/test_models.py
@@ -259,9 +259,9 @@ class FriendRequestTests(TestCase):
         self.assertFalse(self.user2 in self.user1.friend_requests_sent.all())
         self.assertFalse(self.user1.is_friend_with(self.user2))
 
-        # Attempt to send friend request should raise ValueError
-        with self.assertRaises(ValueError):
-            self.user1.send_friend_request(self.user2)
+        # After rejection, user should be able to send another request
+        self.user1.send_friend_request(self.user2)
+        self.assertTrue(self.user2 in self.user1.friend_requests_sent.all())
 
     def test_duplicate_friend_request(self):
         """Test that duplicate friend requests are not created."""
