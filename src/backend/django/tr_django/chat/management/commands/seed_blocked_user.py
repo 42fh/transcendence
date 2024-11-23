602cbe982a5edb50b61461bc2368dca0b6bc0c8b
lmangall <l.mangallon@gmail.com>
2024-11-23 12:11:04 +0100

    Feat(chat): implement seeding of ElonMusk blocking dev

--- src/backend/django/tr_django/chat/management/commands/seed_blocked_user.py
+++ src/backend/django/tr_django/chat/management/commands/seed_blocked_user.py
@@ -4,7 +4,7 @@ from users.models import CustomUser
 
 
 class Command(BaseCommand):
-    help = "Seeding 'dev' blocking 'LexFridman'"
+    help = "Seeding 'dev' blocking 'LexFridman' and 'ElonMusk' blocking 'dev'"
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Seeding 'dev' blocking 'LexFridman'"))
@@ -13,10 +13,24 @@ class Command(BaseCommand):
             dev_user = CustomUser.objects.get(username="dev")
             LexFridman = CustomUser.objects.get(username="LexFridman")
 
-            # Create the BlockedUser entry
+            # BlockedUser entry for 'dev' blocking 'LexFridman'
             BlockedUser.objects.get_or_create(user=dev_user, blocked_user=LexFridman)
 
             self.stdout.write(self.style.SUCCESS("Successfully seeded 'dev' blocking 'LexFridman'"))
+        except CustomUser.DoesNotExist:
+            self.stdout.write(self.style.ERROR("One or both users do not exist"))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"An error occurred: {str(e)}"))
+
+        self.stdout.write(self.style.SUCCESS("Seeding 'ElonMusk' blocking 'dev'"))
+
+        try:
+            ElonMusk = CustomUser.objects.get(username="ElonMusk")
+
+            # BlockedUser entry for 'ElonMusk' blocking 'dev'
+            BlockedUser.objects.get_or_create(user=ElonMusk, blocked_user=dev_user)
+
+            self.stdout.write(self.style.SUCCESS("Successfully seeded 'ElonMusk' blocking 'dev'"))
         except CustomUser.DoesNotExist:
             self.stdout.write(self.style.ERROR("One or both users do not exist"))
         except Exception as e:
