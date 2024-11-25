e04ee04ed9750043fd18d23da94fdca35819f27e
lmangall <l.mangallon@gmail.com>
2024-11-25 23:50:40 +0100

    Fix(get all games in frontend API call

--- src/frontend/js/views/game.js
+++ src/frontend/js/views/game.js
@@ -1,34 +1,38 @@
 import { CONFIG } from "../config/constants.js";
 
 export async function gameHome() {
-    try {
-        console.log("gameHome");
-      const response = await fetch(`${CONFIG.API_BASE_URL}/get_all_games/`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-        
-        console.log("received -->");
-        console.log(response);
-  
+  try {
+    console.log("gameHome");
+    const response = await fetch(`${CONFIG.API_BASE_URL}/api/game/get_all_games/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("received -->");
+      console.log(response);
+      
+      const mainContent = document.getElementById("main-content");
+      mainContent.innerHTML = "";
+
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-  
-      const contentType = response.headers.get("content-type");
-      if (!contentType || !contentType.includes("application/json")) {
-        const text = await response.text();
-        console.error("Non-JSON response received:", text);
-        throw new TypeError("Expected JSON response");
-      }
-  
-      const data = await response.json();
-      console.log("Available games:", data);
-      return data;
-    } catch (error) {
-      console.error("Error fetching available games:", error);
-      throw error;
+        console.log("response not ok");
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const contentType = response.headers.get("content-type");
+    if (!contentType || !contentType.includes("application/json")) {
+      const text = await response.text();
+      console.error("Non-JSON response received:", text);
+      throw new TypeError("Expected JSON response");
+    }
+
+    const data = await response.json();
+    console.log("Available games:", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching available games:", error);
+    throw error;
   }
+}
