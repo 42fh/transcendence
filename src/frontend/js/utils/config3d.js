f5884632c74136c5b3d06665ca63e5578c46a072
Florian Held <flori4nh@gmail.com>
2024-12-02 22:13:50 +0100

    fix: set ws api relative to window location and add required nginx block

--- src/frontend/js/utils/config3d.js
+++ src/frontend/js/utils/config3d.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   API_BASE_URL: "/api", // Base URL for REST API calls
-  WS_BASE_URL: "ws://localhost:8000/ws", // Base WebSocket URL
+  WS_BASE_URL: "/ws", // Base WebSocket URL
   GAME_MODES: {
     POLYGON: "polygon",
     CIRCULAR: "circular",
