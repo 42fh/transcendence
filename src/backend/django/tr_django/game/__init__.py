0cc22773048659854ef71556e36a20e87b07aec8
T.R.A.S.H <hallo>
2024-12-05 22:57:07 +0100

    chore(typo): format typo

--- src/backend/django/tr_django/game/__init__.py
+++ src/backend/django/tr_django/game/__init__.py
@@ -3,4 +3,4 @@ from .polygon.PolygonPongGame import PolygonPongGame
 from .circular.CircularPongGame import CircularPongGame
 
 __all__ = ["AGameManager", "PolygonPongGame", "CircularPongGame"]
-default_app_config = 'game.apps.GameAppConfig'
+default_app_config = "game.apps.GameAppConfig"
