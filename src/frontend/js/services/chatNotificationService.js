b8da0e437bfd80c9afd77082096c3a1f7fd5ce42
lmangall <l.mangallon@gmail.com>
2024-12-05 21:56:59 +0100

    Feat (fetchNotifications)

---
+++ src/frontend/js/services/chatNotificationService.js
@@ -0,0 +1,9 @@
+export async function fetchNotifications() {
+  const response = await fetch("/api/chat/notifications/");
+  console.log("notifications:      ", response);
+  if (!response.ok) throw new Error("Failed to get notifications list");
+
+  const data = await response.json();
+  console.log("Fetched notifications:", data);
+  return data;
+}
