8f64a37c30e7ce4cf0df75fc58cc5970e90c053b
lmangall <l.mangallon@gmail.com>
2024-12-05 22:03:10 +0100

    Feat (notifications API urlpatterns)

--- src/backend/django/tr_django/chat/urls.py
+++ src/backend/django/tr_django/chat/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
     rooms,
     blocked_user,
+    notifications,
 )
 
 urlpatterns = [
     path("users/", rooms, name="rooms"),
     path("blocked_user/", blocked_user, name="blocked_user"),
+    path("notifications/", notifications, name="notifications"),  # GET and POST
 ]
