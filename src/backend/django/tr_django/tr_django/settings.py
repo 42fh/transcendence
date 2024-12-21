e74294b24dd9b88d6d985091e2bb5b75cba8bf2b
Florian Held <flori4nh@gmail.com>
2024-12-22 00:12:38 +0100

    feat: make email configurable

--- src/backend/django/tr_django/tr_django/settings.py
+++ src/backend/django/tr_django/tr_django/settings.py
@@ -108,9 +108,9 @@ SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SAMESITE = "Lax"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_PORT = os.getenv("EMAIL_HOST_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
